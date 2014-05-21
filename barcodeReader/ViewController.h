bd9a5253dea37e35b13f97cc361cb2915c8b5266
CtecTeacher <ctecteacher@H8-T-607853.local>
2014-05-20 19:37:38 -0700

    Initial Commit

---
+++ barcodeReader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  barcodeReader
+//
+//  Created by CtecTeacher on 5/20/14.
+//  Copyright (c) 2014 ABC Adult School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ barcodeReader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  barcodeReader
+//
+//  Created by CtecTeacher on 5/20/14.
+//  Copyright (c) 2014 ABC Adult School. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
